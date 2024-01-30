3ff2a1e5c173f05210de99c6f0d5dc4c5013eb8f
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-30 11:01:05 -0300

    declaring the quantity variable and receiving inputs

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("How many numbers will you enter? ");
+        int quantity = sc.nextInt();
+        double[] positions = new double[quantity];
+        for(int i = 0; i < quantity; i++){
+            System.out.printf("Enter %da number: ", i);
+            positions[i] = sc.nextDouble();
+        }
         sc.close();
     }
 }
